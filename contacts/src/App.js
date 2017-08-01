e9407bbd74d2ca51d9b39aaa0c696a0c75efe4f0
Hafeez Syed <feeziman007@hotmail.com>
2017-08-02 00:43:01 +1000

    added components to render contact list

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ContactList extends Component {
+    render() {
+        const people = this.props.contacts;
+
+        return <ol>
+            {
+                people.map(person => (
+                    <li key={person.name}>{person.name}</li>
+                ))
+            }
+        </ol>
+
+    }
+}
+
 class App extends Component {
   render() {
+      const contacts1 = [
+          { name: 'Michael'},
+          { name: 'Ryan'},
+          { name: 'Tyler'},
+      ];
+      const contacts2 = [
+          { name: 'Harry'},
+          { name: 'James'},
+          { name: 'Andrew'},
+      ];
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +38,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ContactList contacts={contacts1}/>
+        <ContactList contacts={contacts2}/>
       </div>
     );
   }

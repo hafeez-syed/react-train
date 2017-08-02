9a9245ad11bf4b52e8dee218f102141eb1e2f4cb
Hafeez Syed <feeziman007@hotmail.com>
2017-08-03 00:51:12 +1000

    archived started files. Also contacts server added

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,46 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class ContactList extends Component {
-    render() {
-        const people = this.props.contacts;
-
-        return <ol>
-            {
-                people.map(person => (
-                    <li key={person.name}>{person.name}</li>
-                ))
-            }
-        </ol>
-
-    }
-}
 
 class App extends Component {
   render() {
-      const contacts1 = [
-          { name: 'Michael'},
-          { name: 'Ryan'},
-          { name: 'Tyler'},
-      ];
-      const contacts2 = [
-          { name: 'Harry'},
-          { name: 'James'},
-          { name: 'Andrew'},
-      ];
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to the Facebook World of React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ContactList contacts={contacts1}/>
-        <ContactList contacts={contacts2}/>
-      </div>
+      <div>Hello World</div>
     );
   }
 }

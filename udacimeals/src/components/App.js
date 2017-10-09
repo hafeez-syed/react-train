55ee86929b12b1041a1406482c45d3ab50a11012
Hafeez Syed <hafeez.syed@team.telstra.com>
2017-10-09 17:32:13 +1100

    React and Redux is complete

--- udacimeals/src/components/App.js
+++ udacimeals/src/components/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions';
 
 class App extends Component {
+  doThing = () => {
+    this.props.selectRecipe({})
+  };
   render() {
+      console.log('Props', this.props);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          Hello World
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProp(calender) {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    
+    return {
+        calender: dayOrder.map((day) => ({
+            day,
+            meals: Object.keys(calender[day]).reduce((meals, meal) => {
+                meals[meal] = calender[day][meal] ? calender[day][meal] : null;
+                
+                return meals;
+            }, {})
+        }))
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        selectRecipe: (data) => dispatch(addRecipe(data)),
+        remove: (data) => dispatch(removeFromCalendar(data)),
+    }
+}
+
+export default connect(mapStateToProp, mapDispatchToProps)(App);
